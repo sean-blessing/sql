3b46ff1f49d7b34a6321d80db259d03c55586aff
Sean Blessing <snblessing@gmail.com>
2018-03-07 11:26:22 -0500

    added new products to create script

--- mma/mma_create_database.sql
+++ mma/mma_create_database.sql
@@ -19,7 +19,8 @@ INSERT INTO Product VALUES
 (4, 'android', 'Murach''s Android Programming', '57.50'),
 (5, 'html5', 'Murach''s HTML5 and CSS3', '54.50'),
 (6, 'oracle', 'Murach''s Oracle and PL/SQL', '54.50'),
-(7, 'javascript', 'Murach''s JavaScript and jQuery', '54.50');
+(7, 'javascript', 'Murach''s JavaScript and jQuery', '54.50'),
+(8,'test','test description',10.00);
 
 -- create a user and grant privileges to that user
 GRANT SELECT, INSERT, DELETE, UPDATE
